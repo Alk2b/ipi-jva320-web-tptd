b6ad7cc46b36240fd5c5a9785096200d87325ca5
Allan Khebab <allan.khebab@edu.igensia.com>
2026-01-14 12:01:33 +0100

    feat: implémentation de l'accueil, de la liste et du détail des salariés avec Thymeleaf

---
+++ src/main/java/com/ipi/jva320/Controllers/SalarieController.java
@@ -0,0 +1,41 @@
+package com.ipi.jva320.Controllers;
+
+import com.ipi.jva320.model.SalarieAideADomicile;
+import com.ipi.jva320.service.SalarieAideADomicileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class SalarieController {
+
+    @Autowired
+    private SalarieAideADomicileService salarieService;
+
+    @GetMapping(value = "/salaries")
+    public String listSalaries(ModelMap model) {
+        model.put("salaries", salarieService.getSalaries());
+        model.put("nombreSalaries", salarieService.countSalaries());
+        return "list";
+    }
+
+    @GetMapping(value = "/salaries/{id}")
+    public String getSalarie(final ModelMap model,
+                             @PathVariable Long id) {
+        SalarieAideADomicile salarie = salarieService.getSalarie(id);
+        model.put("salarie", salarie);
+        model.put("nombreSalaries", salarieService.countSalaries());
+
+        return "detail_Salarie";
+    }
+
+
+    @GetMapping(value = "/salaries/aide/new")
+    public String newSalarie(final ModelMap model) {
+        model.put("salarie", new SalarieAideADomicile());
+        model.put("nombreSalaries", salarieService.countSalaries());
+        return "detail_Salarie";
+    }
+}
